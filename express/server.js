ccaef522e649a1e7b98f68a10072732fb8ba8ac7
Kevin Felisilda <kevinfelisilda@gmail.com>
2019-03-28 00:55:12 +0800

    echo req

--- express/server.js
+++ express/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  res.write('<pre>' + JSON.stringify(req, null, 2) + '</pre>');
   res.end();
 });
 router.get('/webhook', (req, res) => {
